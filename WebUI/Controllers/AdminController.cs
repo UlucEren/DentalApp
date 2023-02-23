c128a741650f04e73b831f516cb64a49304402a3
ULUÇ EREN <uluceren23@gmail.com>
2023-02-23 14:31:56 +0300

    basecontroller ekleme user listesi getirme ve kullanıcı eklerken mail adresi validate kontrolu

--- WebUI/Controllers/AdminController.cs
+++ WebUI/Controllers/AdminController.cs
@@ -1,18 +1,34 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using WebUI.Models.AppIdentityDb;
 
 namespace WebUI.Controllers
 {
-    public class AdminController : Controller
+    [Authorize(Roles = "admin")]
+    public class AdminController : BaseController
     {
-        [Authorize(Roles = "admin")]
-        public IActionResult Index()
+
+        public AdminController(UserManager<AppUser> userManager, RoleManager<AppRole> roleManager) : base(userManager, null, roleManager)
+        {
+        }
+        public IActionResult Users()
+        {
+            return View(userManager.Users.ToList());
+        }
+        public IActionResult Rol()
         {
             return View();
         }
-    public IActionResult Rol()
+        [HttpPost]
+        public IActionResult CreateUser(string name, string email, string password)
         {
             return View();
+        }
+        public JsonResult MailAvailable(string Email)
+        {            
+                bool available = userManager.Users.Any(x => x.Email == Email);
+                return Json(available);
         }
     }
 }
---
+++ WebUI/Controllers/BaseController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WebUI.Models.AppIdentityDb;
+
+namespace WebUI.Controllers
+{
+    public class BaseController : Controller
+    {
+        protected UserManager<AppUser> userManager { get; }
+        protected SignInManager<AppUser> signInManager { get; }
+
+        protected RoleManager<AppRole> roleManager { get; }
+
+        protected AppUser CurrentUser => userManager.FindByNameAsync(User.Identity.Name).Result;
+        //protected async Task<AppUser?> GetCurrentUserAsync()
+        //{
+        //    if (User?.Identity?.Name != null)
+        //    {
+        //        return await userManager.FindByNameAsync(User.Identity.Name);
+        //    }
+        //    else
+        //    {
+        //        return null;
+        //    }
+            
+        //}
+        //protected async Task<AppUser> CurrentUser()
+        //{
+        //    return await GetCurrentUserAsync();
+        //}
+
+        public BaseController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager, RoleManager<AppRole> roleManager = null)
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+            this.roleManager = roleManager;
+        }
+
+        public void AddModelError(IdentityResult result)
+        {
+            foreach (var item in result.Errors)
+            {
+                ModelState.AddModelError("", item.Description);
+            }
+        }
+    }
+}
