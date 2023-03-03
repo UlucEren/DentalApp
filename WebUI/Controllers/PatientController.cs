03648b9b647da1ce59787ca25de5e42054cf89fd
ULUÇ EREN <uluceren23@gmail.com>
2023-03-03 14:13:02 +0300

    Hasta kayıt ekranı tasarımı

---
+++ WebUI/Controllers/PatientController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+
+namespace WebUI.Controllers
+{
+    [Authorize(Roles = "Hasta Modülü.Read")]
+    public class PatientController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
